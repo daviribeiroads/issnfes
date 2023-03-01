242402c6f524d890e5380fbb2686ac73846407d4
Davi Ribeiro <111585899+daviribeiroads@users.noreply.github.com>
2023-03-01 16:55:21 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function calcular(){
+    let caseOne = Number(tab1.value)
+    let caseTwo = Number(tab2.value)/100;
+    let caseThree = Number(tab3.value)
+    let caseFour = Number(tab4.value)
+    let resultado = ((((caseOne * (caseTwo)) - caseThree) / caseOne) * caseFour).toFixed(2).replace('.',',');
+    x1.innerHTML = `${resultado}`
+}  
